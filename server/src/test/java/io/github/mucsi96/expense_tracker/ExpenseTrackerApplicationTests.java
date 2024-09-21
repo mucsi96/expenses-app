6d0233c06ae16ec25978c817c672cd1d7576fb40
Igor Bari <mucsi96@gmail.com>
2024-09-21 12:59:39 +0200

    initialize server

---
+++ server/src/test/java/io/github/mucsi96/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mucsi96.expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
